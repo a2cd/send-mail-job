4a9f7ca79a92d2cd8a5551dd360ba55556baada3
Kai <ca2pa23187@gmail.com>
2025-09-21 03:21:06 +0800

    Fix cicd function to ensure main.py is terminated before running a new instance

--- scripts/cicd/funcs.sh
+++ scripts/cicd/funcs.sh
@@ -18,5 +18,6 @@ cicd() {
   cd /usr/local/repo/send-mail-job/ && \
   git pull origin main && \
   uv sync && \
+  pkill -f \"python3 main.py\" && \
   (nohup uv run main.py > run.log 2>&1 & disown)'"
 }
