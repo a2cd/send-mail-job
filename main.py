03d3104203f0d4371e122a2f951732b0c4fa8491
Kai <ca2pa23187@gmail.com>
2025-09-21 02:32:46 +0800

    Refactor logging in main.py to use Loguru and configure log rotation; update dependencies in pyproject.toml and uv.lock

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
+import os
+from loguru import logger
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 
 def job():
-    print("定时任务执行！")
+    logger.info("定时任务执行！")
 
 
 if __name__ == "__main__":
+    # 日志目录
+    log_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "logs")
+    os.makedirs(log_dir, exist_ok=True)
+
+    # remove the default handler configuration
+    logger.remove()
+    # 配置Loguru日志：每分钟新建，保留7天
+    logger.add(
+        "logs/job-{time:YYYYMMDD}.log",
+        rotation="00:00",
+        retention="7 days",
+        encoding="utf-8",
+        format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> - [<level>{level}</level>]: {message}",
+    )
+
+    # 启动定时任务
     scheduler = BlockingScheduler()
-    scheduler.add_job(job, "interval", seconds=5)  # 每5秒执行一次
+    scheduler.add_job(job, "interval", seconds=60)  # 每60秒执行一次
     scheduler.start()
